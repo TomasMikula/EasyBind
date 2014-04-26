a326bbe9f22633b39d6187201515d1f6c4fbc14d
Tomas Mikula <tomas.mikula@gmail.com>
2014-04-26 14:25:20 +0200

    Minor refactoring.

--- src/main/java/org/fxmisc/easybind/monadic/FlatMapBinding.java
+++ src/main/java/org/fxmisc/easybind/monadic/FlatMapBinding.java
@@ -7,17 +7,21 @@ import javafx.beans.WeakInvalidationListener;
 import javafx.beans.binding.ObjectBinding;
 import javafx.beans.value.ObservableValue;
 
+import org.fxmisc.easybind.Subscription;
+
 class FlatMapBinding<T, U> extends ObjectBinding<U> implements MonadicBinding<U> {
     private final ObservableValue<T> src;
     private final Function<? super T, ObservableValue<U>> mapper;
 
+    // need to retain strong reference to listeners, so that they don't get garbage collected
+    private final InvalidationListener srcListener = obs -> srcInvalidated();
     private final InvalidationListener mappedListener = obs -> mappedInvalidated();
+
+    private final InvalidationListener weakSrcListener = new WeakInvalidationListener(srcListener);
     private final InvalidationListener weakMappedListener = new WeakInvalidationListener(mappedListener);
 
-    private final InvalidationListener srcListener = obs -> srcInvalidated();
-    private final InvalidationListener weakSrcListener = new WeakInvalidationListener(srcListener);
-
     private ObservableValue<U> mapped = null;
+    private Subscription mappedSubscription = null;
 
     public FlatMapBinding(ObservableValue<T> src, Function<? super T, ObservableValue<U>> f) {
         this.src = src;
@@ -26,34 +30,40 @@ class FlatMapBinding<T, U> extends ObjectBinding<U> implements MonadicBinding<U>
     }
 
     @Override
-    public void dispose() {
+    public final void dispose() {
         src.removeListener(weakSrcListener);
         disposeMapped();
     }
 
     @Override
-    protected U computeValue() {
+    protected final U computeValue() {
         if(mapped == null) {
             T baseVal = src.getValue();
             if(baseVal == null) {
                 return null;
             } else {
                 mapped = mapper.apply(baseVal);
-                mapped.addListener(weakMappedListener);
+                mappedSubscription = observeMapped(mapped);
             }
         }
         return mapped.getValue();
     }
 
-    private void mappedInvalidated() {
-        invalidate();
+    protected Subscription observeMapped(ObservableValue<U> mapped) {
+        mapped.addListener(weakMappedListener);
+        return () -> mapped.removeListener(weakMappedListener);
     }
 
     private void disposeMapped() {
         if(mapped != null) {
-            mapped.removeListener(weakMappedListener);
+            mappedSubscription.unsubscribe();
+            mappedSubscription = null;
             mapped = null;
         }
+    }
+
+    private void mappedInvalidated() {
+        invalidate();
     }
 
     private void srcInvalidated() {
